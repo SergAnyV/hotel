5091049d71f2bdce5714d9dc033225e5a688dd0d
SergAnyV <anikeevvlg@yandex.ru>
2025-06-08 16:51:38 +0300

    init project

---
+++ src/test/java/com/asv/hotel/HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asv.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
