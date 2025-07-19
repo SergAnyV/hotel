33f9041c64e6573643b257c0680f52a22997cfc8
SergeiVA <anikeevvlg@yandex.ru>
2025-07-19 14:47:22 +0300

    смена профиля для тестов

--- src/main/java/com/asv/hotel/controllers/BookingController.java
+++ src/main/java/com/asv/hotel/controllers/BookingController.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
