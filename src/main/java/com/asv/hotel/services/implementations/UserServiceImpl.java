b4198467583ecbade8f0101e657c57e5dfba6e1d
SergeiVA <anikeevvlg@yandex.ru>
2025-09-28 14:30:54 +0300

    удаление пасвордендкодер в сервисном

--- src/main/java/com/asv/hotel/services/implementations/UserServiceImpl.java
+++ src/main/java/com/asv/hotel/services/implementations/UserServiceImpl.java
@@ -12,7 +12,6 @@ import com.asv.hotel.services.UserTypeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserInternalService {
     private final UserRepository userRepository;
     private final UserTypeService userTypeService;
-    private final PasswordEncoder passwordEncoder;
+
 
 
     @Transactional
